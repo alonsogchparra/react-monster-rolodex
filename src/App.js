7713b0a3de5529c11515a4024eb7a43f639c3350
Alonso Parra <alonsogparra@gmail.com>
2020-03-31 10:32:16 -0400

    App rebuilt for this project with a class to add state on it and loading the monsters's name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    monsters: [],
+    searchField: ''
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ monsters: users }))
+    .catch(error => console.log(error));
+  }
+
+
+  render() {
+    console.log('monsters', this.state.monsters);
+
+    const { monsters } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <CardList monsters={monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
