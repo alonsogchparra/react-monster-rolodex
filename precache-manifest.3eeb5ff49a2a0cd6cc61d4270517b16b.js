a53a2125457c77f4eb5697c1dd5271da0eaff116
Alonso Parra <alonsogparra@gmail.com>
2020-03-31 12:57:28 -0400

    Updates

---
+++ precache-manifest.3eeb5ff49a2a0cd6cc61d4270517b16b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50963d6a98417a1c1edc27aa3a8fdee1",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "1c8c172c8465c0d423cf",
+    "url": "/react-monster-rolodex/static/css/main.45164e6c.chunk.css"
+  },
+  {
+    "revision": "f60a5352a19f3072690e",
+    "url": "/react-monster-rolodex/static/js/2.8c530613.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.8c530613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8c172c8465c0d423cf",
+    "url": "/react-monster-rolodex/static/js/main.7df9a47c.chunk.js"
+  },
+  {
+    "revision": "a8c5f7358ca806a31557",
+    "url": "/react-monster-rolodex/static/js/runtime-main.6f127034.js"
+  }
+]);
